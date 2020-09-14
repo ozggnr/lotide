0fd5be188ad5f21bfcb4d47a52ab42766e08d65a
ozgeguner <ozgecaylioglu@gmail.com>
2020-09-14 21:51:29 +0000

    assertion

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return String.fromCodePoint(0x1F60C) + "Assertion Passed: " + actual + " === " + expected;
+    return `${String.fromCodePoint(0x1F60C)}` + "Assertion Passed: " + `${actual}` + " === " + `${expected}`;
   } else {
-    return String.fromCharCode(0x26D4) + "Assertion failed: " + actual + " !== " + expected;
+    return `${String.fromCharCode(0x26D4)}` + "Assertion failed: " + `${actual} `+ " !== " + `${expected}`;
   }
 };
 
