72e8f900e76639312d997e9f1fa7cd4fcc47bfba
ozgeguner <ozgecaylioglu@gmail.com>
2020-09-14 21:01:39 +0000

    first commit with emoji :)

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return String.fromCodePoint(0x1F60C) + "Assertion Passed: " + actual + " === " + expected;
+  } else {
+    return String.fromCharCode(0x26D4) + "Assertion failed: " + actual + " !== " + expected;
+  }
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual("Lighthouse", "Lighthouse"));
+console.log(assertEqual(2,3));
