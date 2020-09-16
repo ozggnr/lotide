7e7cd6db7395aaa1e06fb1bf07749643a9f561e4
ozgeguner <ozgecaylioglu@gmail.com>
2020-09-16 20:07:24 +0000

    count letters

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${String.fromCodePoint(0x1F60C)}` + "Assertion Passed: " + `${actual}` + " === " + `${expected}`);
+  } else {
+    console.log(`${String.fromCharCode(0x26D4)}` + "Assertion failed: " + `${actual} `+ " !== " + `${expected}`);
+  }
+};
+const countLetters = function(letter) {
+  let result = {};
+  for (obj of letter) { //take each letter from given string
+    if (obj !== " ") {//skip the spaces
+      if (result[obj]){ //if obj key is already in the result object, then increase by 1.
+        result[obj] += 1;
+      } else {// if there is not an obj in the result object then assign value 1 for this obj key.
+        result[obj] = 1
+      }
+    }
+  }
+  return result;
+}
+
+// console.log(countLetters("lighthouse in the house"))
+const resultFinal = countLetters("lighthouse in the house")
