9930481c300aefc797e27d4faeeb1d9e052d3f04
ozgeguner <ozgecaylioglu@gmail.com>
2020-09-21 19:01:08 +0000

    middle test

--- middle.js
+++ middle.js
@@ -1,29 +1,6 @@
-const eqArrays = function(arr1, arr2) {
-  //first and foremost the length of the arrays should be equal
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      // compare each element one to one if they have the same value and same type
-      if (arr1[i] === arr2[i]) {
-        //if they equal strictly, we can continue with the next element
-        continue;
-      } else {
-          return false;
-      }
-    }
-    return true;
-  } else {
-      return false;
-  }
-}
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
-const assertArraysEqual = function (left, right) {
-  if (eqArrays(left, right)) { //if the function eqArrays return true, print out as "Assertion Passed" otherwise it will show "Assertion failed"
-    console.log(`${String.fromCodePoint(0x1F60C)}` + "Assertion Passed: " + `${left}` + " === " + `${right}`);
-  }
-  else {
-    console.log(`${String.fromCharCode(0x26D4)}` + "Assertion failed: " + `${left}` + " !== " + `${right}`);
-  }
-}
 const middle = function(array) {
   let list = [];
   const minIndex = array.length/2 - 1;
@@ -38,8 +15,5 @@ const middle = function(array) {
   }
   return list;
 }
-assertArraysEqual(middle([1,2]),[])
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]) // => [3, 4]
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle.js");
+
+assertArraysEqual(middle([1,2]),[])
+assertArraysEqual(middle([1, 2, 3]),[2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
+assertArraysEqual(middle([1, 2, 3, 4]),[2,3]) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]) // => [3, 4]
